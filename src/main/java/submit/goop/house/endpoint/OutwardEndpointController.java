14570d03eb2136b39e60b1710386c474c99ea257
TheLickIn13Keys <bardiaanvari10@gmail.com>
2022-04-19 22:35:07 -0700

    added endpoint dupe check

--- src/main/java/submit/goop/house/endpoint/OutwardEndpointController.java
+++ src/main/java/submit/goop/house/endpoint/OutwardEndpointController.java
@@ -12,66 +12,74 @@ import submit.goop.house.data.service.UserRepository;
 import submit.goop.house.data.service.UserService;
 
 import java.util.Collections;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/endpoint")
 public class OutwardEndpointController {
 
+    private UserService userService;
     private PasswordEncoder passwordEncoder;
     private UserRepository userRepository;
 
-    public OutwardEndpointController(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    public OutwardEndpointController(PasswordEncoder passwordEncoder, UserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
     }
 
     @RequestMapping(value = "/discordID={id}&password={pass}&discordTag={tag}&discordPicture={pic}", produces = MediaType.APPLICATION_JSON_VALUE)
     //@JsonView(View.Public.class)
     public ResponseEntity<String> get(@PathVariable("id") String id, @PathVariable("tag") String tag, @PathVariable("pic") String pic, @PathVariable("pass") String pass) {
-        try {
-            User user = new User();
-            user.setName(tag
-                    .replace("^space^", " ")
-                    .replace("^hash^", "#")
-                    .replace("^lt^", "<")
-                    .replace("^gt^", ">")
-                    .replace("^amp^", "&")
-                    .replace("^pipe^", "|")
-                    .replace("^slash^", "/")
-                    .replace("^backslash^", "\\")
-                    .replace("^question^", "?")
-                    .replace("^star^", "*")
-                    .replace("^quote^", "\"")
-                    .replace("^apos^", "'")
-                    .replace("^backtick^", "`")
-                    .replace("^dollar^", "$")
-                    .replace("^at^", "@")
-                    .replace("^exclamation^", "!")
-            );
-            user.setUsername(id);
-            user.setHashedPassword(passwordEncoder.encode(pass
-                    .replace("^space^", " ")
-                    .replace("^hash^", "#")
-                    .replace("^lt^", "<")
-                    .replace("^gt^", ">")
-                    .replace("^amp^", "&")
-                    .replace("^pipe^", "|")
-                    .replace("^slash^", "/")
-                    .replace("^backslash^", "\\")
-                    .replace("^question^", "?")
-                    .replace("^star^", "*")
-                    .replace("^quote^", "\"")
-                    .replace("^apos^", "'")
-                    .replace("^backtick^", "`")
-                    .replace("^dollar^", "$")
-                    .replace("^at^", "@")
-                    .replace("^exclamation^", "!")));
-            user.setProfilePictureUrl("https://" + pic.replace("-", "/"));
-            user.setRoles(Collections.singleton(Role.USER));
-            userRepository.save(user);
-            return ResponseEntity.ok("Successfully created user");
-        } catch (Exception e) {
-            return ResponseEntity.ok("Failed to create user");
+        User possibleUser = userService.findByUsername(id);
+        if (possibleUser == null) {
+            try {
+                User user = new User();
+                user.setName(tag
+                        .replace("^space^", " ")
+                        .replace("^hash^", "#")
+                        .replace("^lt^", "<")
+                        .replace("^gt^", ">")
+                        .replace("^amp^", "&")
+                        .replace("^pipe^", "|")
+                        .replace("^slash^", "/")
+                        .replace("^backslash^", "\\")
+                        .replace("^question^", "?")
+                        .replace("^star^", "*")
+                        .replace("^quote^", "\"")
+                        .replace("^apos^", "'")
+                        .replace("^backtick^", "`")
+                        .replace("^dollar^", "$")
+                        .replace("^at^", "@")
+                        .replace("^exclamation^", "!")
+                );
+                user.setUsername(id);
+                user.setHashedPassword(passwordEncoder.encode(pass
+                        .replace("^space^", " ")
+                        .replace("^hash^", "#")
+                        .replace("^lt^", "<")
+                        .replace("^gt^", ">")
+                        .replace("^amp^", "&")
+                        .replace("^pipe^", "|")
+                        .replace("^slash^", "/")
+                        .replace("^backslash^", "\\")
+                        .replace("^question^", "?")
+                        .replace("^star^", "*")
+                        .replace("^quote^", "\"")
+                        .replace("^apos^", "'")
+                        .replace("^backtick^", "`")
+                        .replace("^dollar^", "$")
+                        .replace("^at^", "@")
+                        .replace("^exclamation^", "!")));
+                user.setProfilePictureUrl("https://" + pic.replace("-", "/"));
+                user.setRoles(Collections.singleton(Role.USER));
+                userRepository.save(user);
+                return ResponseEntity.ok("Successfully created user");
+            } catch (Exception e) {
+                return ResponseEntity.ok("Failed to create user");
+            }
+        } else {
+            return ResponseEntity.ok("User already exists");
         }
     }
 }
